8168a780d774d7e148297bfefa3ca00b12c3a3c1
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2023-10-17 22:08:32 +0200

    Bump up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.elasticrock.exhibition"
         minSdk = 21
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.0"
+        versionCode = 6
+        versionName = "1.0.0-beta"
         vectorDrawables {
             useSupportLibrary = true
         }
